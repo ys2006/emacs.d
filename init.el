ecb37a6886ba47119d272295cf9ac5dd35b55629
ys2006 <yinshuo335@hotmail.com>
2017-07-28 10:03:22 -0700

    use us proxy

--- init.el
+++ init.el
@@ -15,10 +15,16 @@
 (setq emacs-load-start-time (current-time))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
+;;(setq url-proxy-services
+;;      '(("http"     . "cn-proxy.jp.oracle.com:80")
+;;        ("https"    . "cn-proxy.jp.oracle.com:80")
+;;        ("ftp"      . "cn-proxy.jp.oracle.com:80")
+;;        ("no_proxy" . "^\\(localhost\\|10.*\\)")))
+
 (setq url-proxy-services
-      '(("http"     . "cn-proxy.jp.oracle.com:80")
-        ("https"    . "cn-proxy.jp.oracle.com:80")
-        ("ftp"      . "cn-proxy.jp.oracle.com:80")
+      '(("http"     . "www-proxy.us.oracle.com:80")
+        ("https"    . "www-proxy.us.oracle.com:80")
+        ("ftp"      . "www-proxy.us.oracle.com:80")
         ("no_proxy" . "^\\(localhost\\|10.*\\)")))
 
 (setq default-directory "~/")
