0c02cafbcfca9d4c6764adab8b08ae3040e8b488
ys2006 <yinshuo335@hotmail.com>
2017-07-28 09:33:49 -0700

    fix init-magit bug

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,3 +1,4 @@
 (custom-set-faces
     '(magit-diff-added ((t (:background "black" :foreground "green3"))))
     '(magit-diff-removed ((t (:background "black" :foreground "red3")))))
+(provide 'init-magit)
