d5dfcd80a268ecb5ba7081b9afe4a19cc9cf6ea6
dylan.yin <dylan.yin@oracle.com>
2017-12-01 20:49:07 +0800

    update easy hugo

--- init.el
+++ init.el
@@ -15,11 +15,11 @@
 (setq emacs-load-start-time (current-time))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
-;;(setq url-proxy-services
-;;     '(("http"     . "cn-proxy.jp.oracle.com:80")
-;;       ("https"    . "cn-proxy.jp.oracle.com:80")
-;;       ("ftp"      . "cn-proxy.jp.oracle.com:80")
-;;       ("no_proxy" . "^\\(localhost\\|10.*\\)")))
+;; (setq url-proxy-services
+;;      '(("http"     . "cn-proxy.jp.oracle.com:80")
+;;        ("https"    . "cn-proxy.jp.oracle.com:80")
+;;        ("ftp"      . "cn-proxy.jp.oracle.com:80")
+;;        ("no_proxy" . "^\\(localhost\\|10.*\\)")))
 
 ;;(setq url-proxy-services
 ;;     '(("http"     . "www-proxy.us.oracle.com:80")
@@ -143,6 +143,7 @@ cygwin-mount-cygwin-bin-directory "c:/Users/dyyin/.babun/cygwin/bin")
   (require 'init-keyfreq)
   (require 'init-httpd)
   (require 'init-magit)
+  (require 'init-easy-hugo)
 
   ;; projectile costs 7% startup time
 
--- lisp/init-easy-hugo.el
+++ lisp/init-easy-hugo.el
@@ -6,4 +6,20 @@
 (setq easy-hugo-previewtime "300")
 (define-key global-map (kbd "C-c C-e") 'easy-hugo)
 
+;; (setq easy-hugo-bloglist
+;;       ;; blog2 setting
+;;       '(((easy-hugo-basedir . "~/src/github.com/masasam/hugo2/")
+;;          (easy-hugo-url . "http://example2.com")
+;;          (easy-hugo-sshdomain . "myblogdomain")
+;;          (easy-hugo-root . "/home/hugo/"))
+;;         ;; blog3 setting
+;;         ((easy-hugo-basedir . "~/src/github.com/masasam/hugo3/")
+;;          (easy-hugo-url . "http://example3.net")
+;;          (easy-hugo-amazon-s3-bucket-name . "yours3bucketname"))
+;;         ;; blog4 setting
+;;         ((easy-hugo-basedir . "~/src/github.com/masasam/hugo4/")
+;;          (easy-hugo-url . "http://example4.net")
+;;          (easy-hugo-google-cloud-storage-bucket-name . "yourGCPbucketname")
+;;          (easy-hugo-image-directory . "img"))))
+;;:bind ("C-c C-e" . easy-hugo)
 (provide 'init-easy-hugo)
