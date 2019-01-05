281e9981a2a2271dd99ec7c7ac86e3892fe3a5e0
dylan_yin <dylan.yin@oracle.com>
2019-01-05 14:55:38 +0800

    add bookmarks and golang,projectile init

---
+++ lisp/init-projectile.el
@@ -0,0 +1,15 @@
+(require 'projectile)
+ 
+;; 默认全局使用
+(projectile-global-mode)
+ 
+;; 默认打开缓存
+(setq projectile-enable-caching t)
+ 
+;;【现在就像浏览自己本地文件目录一样，也可以编辑响应缓慢的问题可以通过添加这行来解决】
+(setq projectile-mode-line "Projectile") 
+ 
+;; 使用f5键打开默认文件搜索
+(global-set-key [f5] 'projectile-find-file)
+
+(provide 'init-projectile)
